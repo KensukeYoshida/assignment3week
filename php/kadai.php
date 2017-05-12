dbd5a1962c0c75e70b3717678d4b8c37ad4a1e11
kensuke yoshida <kenban03@gmail.com>
2017-05-12 13:53:39 +0800

    仮

--- php/kadai.php
+++ php/kadai.php
@@ -1,10 +1,33 @@
+ <?php 
+if (date("Hi") >= "0600" && date("Hi") <= "1100") : 
+?>
+  <!-- 06:00～11:00 に表示させたい文字列-->
+  おはようございます、ゲストさん
+ 
+<?php elseif (date("Hi") >= "1101" && date("Hi") <= "1759") : ?>
+  <!--21:00～03:59 に表示させたい文字列-->
+  こんにちは、ゲストさん
+  <?php echo "こんにちは、ゲストさん"; ?>
+
+  <?php elseif (date("Hi") >= "1800" && date("Hi") <= "0559") : ?>
+  <!--21:00～03:59 に表示させたい文字列-->
+  <?php echo "文字列"; ?>
+ 
+<?php else : ?>
+  <!--上記時間以外に表示させたい文字列-->
+  <?php echo "文字列"; ?>
+ 
+<?php endif; ?>
+  <?php echo "文字列"; ?>
+ 
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
     <title>kadai</title>
-    <link href="css/kadai.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link href="../css/kadai.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
   </head>
 
   <body>
@@ -14,7 +37,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 content-margin-top box">
-                <div>こんにちは、ゲストさん！</div><hr></hr>
+              <div>
+                <?php
+                    $time = intval(date('Hi'));
+                    if ("0600" <= $time && $time <= "1100") { // 6時～11時の時間帯のとき 
+                    echo "おはようございます、ゲストさん";
+                    } elseif ("1101" <= $time && $time <= "1759") { // 12時〜20時の時間帯のとき
+                    echo "こんにちわ、ゲストさん";
+                    } else{ // 18時〜5時59分の時間帯のとき 
+                    echo "こんばんは、ゲストさん";
+                    }
+                    ?>
+                </div><hr></hr>
                 <div>2016年10月の日記</div><hr></hr>
                 <div>2016年9月の日記</div><hr></hr>
                 <div>2016年8月の日記</div><hr></hr>
