c8f880bdacaf0ed6c3e4bac1840b359f995ebd2f
kensuke yoshida <kenban03@gmail.com>
2017-05-12 15:09:08 +0800

    日付によって表示変更と時間帯によって表示変更

--- php/kadai.php
+++ php/kadai.php
@@ -1,24 +1,14 @@
- <?php 
-if (date("Hi") >= "0600" && date("Hi") <= "1100") : 
-?>
-  <!-- 06:00～11:00 に表示させたい文字列-->
-  おはようございます、ゲストさん
- 
-<?php elseif (date("Hi") >= "1101" && date("Hi") <= "1759") : ?>
-  <!--21:00～03:59 に表示させたい文字列-->
-  こんにちは、ゲストさん
-  <?php echo "こんにちは、ゲストさん"; ?>
-
-  <?php elseif (date("Hi") >= "1800" && date("Hi") <= "0559") : ?>
-  <!--21:00～03:59 に表示させたい文字列-->
-  <?php echo "文字列"; ?>
- 
-<?php else : ?>
-  <!--上記時間以外に表示させたい文字列-->
-  <?php echo "文字列"; ?>
- 
-<?php endif; ?>
-  <?php echo "文字列"; ?>
+ <?php
+        $time = intval(date('Hi'));
+        if ("0600" <= $time && $time <= "1100") { // 6時～11時の時間帯のとき 
+        echo "おはようございます、ゲストさん";
+        } elseif ("1101" <= $time && $time <= "1759") { // 12時〜20時の時間帯のとき
+        echo "こんにちわ、ゲストさん";
+        } else{ // 18時〜5時59分の時間帯のとき 
+        echo "こんばんは、ゲストさん";
+        }
+        ?>
+    var_dump
  
 
 <!DOCTYPE html>
@@ -29,7 +19,6 @@ if (date("Hi") >= "0600" && date("Hi") <= "1100") :
     <link href="../css/kadai.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
   </head>
-
   <body>
     <div id="header">
       <header>NexSeed Diary</header>
@@ -40,18 +29,49 @@ if (date("Hi") >= "0600" && date("Hi") <= "1100") :
               <div>
                 <?php
                     $time = intval(date('Hi'));
-                    if ("0600" <= $time && $time <= "1100") { // 6時～11時の時間帯のとき 
+                    if ("2100" <= $time && $time <= "0200") { // 日本時間で6時～11時の時間帯のとき 
                     echo "おはようございます、ゲストさん";
-                    } elseif ("1101" <= $time && $time <= "1759") { // 12時〜20時の時間帯のとき
+                    } elseif ("0201" <= $time && $time <= "0859") { // 日本時間11時〜17時59分の時間帯のとき
                     echo "こんにちわ、ゲストさん";
-                    } else{ // 18時〜5時59分の時間帯のとき 
+                    } else{ // 日本時間 18時〜5時59分の時間帯のとき 
                     echo "こんばんは、ゲストさん";
                     }
                     ?>
                 </div><hr></hr>
-                <div>2016年10月の日記</div><hr></hr>
-                <div>2016年9月の日記</div><hr></hr>
-                <div>2016年8月の日記</div><hr></hr>
+                <div>
+                    <?php echo
+                        date("Y年m月",mktime(
+                        0,//時
+                        0,//分
+                        0,//秒
+                        date("m"),
+                        date("d"),
+                        date("Y")
+                        ));
+                    ?>の日記です
+                </div><hr></hr>
+                <div> 
+                    <?php echo
+                        date("Y年m月",mktime(
+                        0,//時
+                        0,//分
+                        0,//秒
+                        date("m") -1,
+                        date("d"),
+                        date("Y")
+                        ));
+                    ?>の日記です</div><hr></hr>
+                <div> 
+                    <?php echo
+                        date("Y年m月",mktime(
+                        0,//時
+                        0,//分
+                        0,//秒
+                        date("m")-2,
+                        date("d"),
+                        date("Y")
+                        ));
+                    ?>の日記です</div><hr></hr>
             </div>
           <div class="col-md-8 content-margin-top2 box">
             <div class="msg">
